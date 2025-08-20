8c651f277923f9cb3995482ed133095273cc7246
Roshan Pal <roshanpal18172@gmail.com>
2025-08-20 16:14:49 +0530

    Create db.js
    to connect mongodb create db.js

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost:27017/ecampus?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURI);
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit process with failure
+  }
+}
+
+module.exports = connectDB;
