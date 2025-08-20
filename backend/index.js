d42228863c315d3b06b682430cb2fc56f85c3ea7
Roshan Pal <roshanpal18172@gmail.com>
2025-08-20 16:16:26 +0530

    Create index.js
    creating indesx.js to connect database and the entry point

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const connectDB = require('/db')
+connectDB();
+const express = require('express')
+const app = express()
+const PORT = 3000;
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Welcome to eCampus')
+})
+
+app.listen(PORT, () => {
+    console.log(`eCampus server is running on port http://localhost:${PORT}`)
+})
