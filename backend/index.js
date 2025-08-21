d2739080c0d96fa6535fdd17ee0a69d5630f5403
Roshan Pal <roshanpal18172@gmail.com>
2025-08-21 22:17:11 +0530

    completed backend
    backend is completed

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ connectDB();
 const express = require('express')
 const app = express()
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.json());
+
+const cors = require('cors');
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome to eCampus')
